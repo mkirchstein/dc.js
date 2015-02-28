144506eb7ffa62d3ead9428e84e908dc5d02ad56
mkirchstein <mkirchstein@gmail.com>
2015-02-28 00:33:18 -0600

    WIP - Earnings bubble and pie charts

---
+++ datagen/data.rb
@@ -0,0 +1,59 @@
+require 'date'
+require 'tempfile'
+require 'csv'
+
+headers = ['date', 'uid', 'gender', 'age', 'incentive', 'amount', 'amount_possible']
+rows = []
+users = []
+num_users = 10
+incentives = [
+  { name: 'steps', label: 'Step Tracking', randThreshold: 0.2 }, 
+  { name: 'sleep', label: 'Sleep Tracking', randThreshold: 0.5 },
+  { name: 'food', label: 'Food Tracking', randThreshold: 0.7 }
+]
+genders = ['female','male']
+daily_max = 5
+daily_tracked_rate = 0.5
+start_date = Date.new(2014, 01, 01)
+end_date = Date.new(2014, 12, 31)
+
+num_users.times do |i|
+  user = { 
+    uid: (0...10).map { ('a'..'z').to_a[rand(26)] }.join,
+    gender: genders[rand.round],
+    age: 25 + (rand * 10).round + (rand * 10).round
+  }
+  # puts user
+  users << user
+end
+
+puts headers.join(',')+"\n"
+
+(start_date..end_date).each do |date|
+  # puts date
+  users.each do |user|
+    incentives.each do |incentive|
+      if rand > incentive[:randThreshold]
+        amount = rand(daily_max)
+        row = [date, user[:uid], user[:gender], user[:age], incentive[:name], amount, daily_max]
+        # rows << row
+        puts row.join(',')+"\n"
+      end
+    end
+  end
+end
+
+# puts rows.join(',')+"\n"
+
+# file = Tempfile.new('~/Dev/dc.js/tmp/incentives.csv')
+# file = Tempfile.new('incentives')
+# puts file.path
+# CSV.open(file, 'w') do |csv|
+#   csv << headers
+#   rows.each do |row|
+#     csv << row
+#   end
+# end
+
+
+
